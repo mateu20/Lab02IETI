1a9a48f1a441ed3f21dd1fe5faaa1dfbb4a140d0
Mateo Gonzalez <matteu20@hotmail.com>
2020-08-23 11:07:31 -0500

    fin lab

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { Redirect } from 'react-router'
+import Button from '@material-ui/core/Button';
 
 
 
@@ -17,11 +18,13 @@ class App extends Component {
         super(props);
         this.state = { isLoggedIn: JSON.parse(localStorage.getItem('recordar')) };  
         const isLogged = JSON.parse(localStorage.getItem('recordar'));
-        this.state = { isLoggedIn: isLogged }      
+        this.state = { isLoggedIn: isLogged }   
+        this.handleClick = this.handleClick.bind(this);
     }
     changeView() {
         const newLogin = this.state.isLoggedIn === false ? true : false;
         this.setState({ isLoggedIn: newLogin });
+        
     }
 
     render() {
@@ -46,13 +49,44 @@ class App extends Component {
                     <br />
                     <br />                    
                     <h4>Login: {localStorage.getItem('recordar')}</h4>
+
+                    <Button
+                        type="submit"
+                        size= "small"
+                        variant="contained"
+                        color="primary"
+                        className="submit"
+                        style={{ top: '35%', right: '-26%' }}
+                        onClick={this.handleClick}
+                        
+                    >
+                        Logout
+                        
+                            </Button>
+
                     <div>
+                        
                         <Route component={!this.state.isLoggedIn ? LoginView : TodoAppView} />
                     </div>
                 </div>
             </Router>
         );
-    }      
+    }
+    handleClick(e) {
+        
+
+        
+        if (localStorage.getItem('recordar') == 'true') {
+            localStorage.setItem("recordar", false)
+            this.changeView();
+        }
+        
+
+        }
+
+
 }
+
+
 
 export default App;
